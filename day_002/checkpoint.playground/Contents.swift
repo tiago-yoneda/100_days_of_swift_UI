ced5252275d6fede1d8fa166a06bd6416e749248
Tiago Yoneda <tiago.yoneda@meliuz.com.br>
2023-03-18 16:56:54 -0300

    chore: Add day 2 files
    Signed-off-by: Tiago Yoneda <tiago.yoneda@meliuz.com.br>

---
+++ day_002/checkpoint.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+// Build a simple Celsius to Fahrenheit calculator
+
+let celsiusTemperature = 36
+let fahrenheitTemperature = ( celsiusTemperature * 9 / 5 ) + 32
+
+let message = "If you want to convert, \(celsiusTemperature)°C is equal to \(fahrenheitTemperature)°F. Have a nice day."
+
+print(message)
